d76b6c43314a0757ef386ebe91f5a5a7602ab531
Christina Yun <poncheeyun@yahoo.com>
2021-10-06 14:39:36 -0700

    Finished findSteps

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -40,27 +40,12 @@ async function findById(scheme_id) {
   return layeredScheme;
 }
 
-function findSteps(scheme_id) { // EXERCISE C
-  /*
-    1C- Build a query in Knex that returns the following data.
-    The steps should be sorted by step_number, and the array
-    should be empty if there are no steps for the scheme:
-
-      [
-        {
-          "step_id": 5,
-          "step_number": 1,
-          "instructions": "collect all the sheep in Scotland",
-          "scheme_name": "Get Rich Quick"
-        },
-        {
-          "step_id": 4,
-          "step_number": 2,
-          "instructions": "profit",
-          "scheme_name": "Get Rich Quick"
-        }
-      ]
-  */
+function findSteps(scheme_id) {
+    return db('steps as st')
+      .leftJoin('schemes as sc', 'st.scheme_id', 'sc.scheme_id')
+      .select('st.step_id', 'st.step_number', 'st.instructions', 'sc.scheme_name')
+      .where('sc.scheme_id', scheme_id)
+      .orderBy('st.step_number', 'asc')
 }
 
 function add(scheme) { // EXERCISE D
